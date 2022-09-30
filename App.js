8abd25e00a976e0cbbe1943679a7c7c3f01232f7
gonzalodutto <gonzalodutto@gmail.com>
2022-09-30 22:31:29 +0200

    data fetched and rendered in screen

--- App.js
+++ App.js
@@ -4,9 +4,30 @@ import { useEffect, useState } from "react";
 import productsApi from "./apis/Products";
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+
+  console.log(products);
+
+  useEffect(() => {
+    try {
+      const getProducts = async () => {
+        const response = await productsApi.get(
+          `https://bukloo.herokuapp.com/sheetstore/ferdulu`
+        );
+        // console.log(response);
+        setProducts(response.data);
+      };
+      getProducts();
+    } catch (e) {
+      console.log(e.message);
+    }
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {products
+        ? products.map((product, i) => <Text key={i}>{product.title} </Text>)
+        : ""}
       <StatusBar style="auto" />
     </View>
   );
